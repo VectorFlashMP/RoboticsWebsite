c3054393ed473131b59610a760f50c75768dd186
VectorFlashMP <pabremad26@7hills.org>
2025-10-05 14:02:41 -0400

    Update schema_clean.sql

--- clock_in_clock_out_system/basic_version/schema_clean.sql
+++ clock_in_clock_out_system/basic_version/schema_clean.sql
@@ -1,24 +1,17 @@
-/*M!999999\- enable the sandbox mode */ 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `logs` (
-  `log_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `action` enum('entry','exit') NOT NULL,
-  `time` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`log_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) 
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_code` varchar(100) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `subteam` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_code` (`user_code`)
-) 
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_code VARCHAR(100) UNIQUE,
+    name VARCHAR(100),
+    subteam VARCHAR(100)
+);
+
+
+CREATE TABLE logs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    action ENUM('entry','exit') NOT NULL,
+    time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
