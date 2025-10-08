5f6307cc36744160559fff7ccf253d66bad44504
Madhav <72686794+DiracDeltaM@users.noreply.github.com>
2025-10-08 12:07:45 -0400

    Update htmlfile.php

--- clock_in_clock_out_system/basic_version/htmlfile.php
+++ clock_in_clock_out_system/basic_version/htmlfile.php
@@ -13,8 +13,18 @@
   <form action="log.php" method="POST">
     <input type="text" name="code" required>
     <br>
-    <button type="submit" name="action" value="entry">Enter</button>
-    <button type="submit" name="action" value="exit">Exit</button>
+    <button type="submit" name="action" value="entry">Clock-in</button>
+    <button type="submit" name="action" value="exit">Clock-out</button>
   </form>
+  <h3>OR</h3>
+  <br>
+  <br>
+<p style="color:red;">Manually enter your time in case you forgot to clock in</p>
+<form action="log.php" method="POST">
+  <input type="text" name="code" placeholder="Your code" required>
+  <input type="datetime-local" name="entrytime">
+   <br>
+  <button type="submit" name="manualcin" value="1">Enter time manually</button>
+</form>
 </body>
 </html>
